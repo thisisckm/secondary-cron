951adf11bd47c1e6667476eadec913e56a63b743
Chandrakumar Murugesan <cmurugesan@chameleoncodewing.co.uk>
2022-08-22 21:14:26 +0000

    Removing local docker image after publish

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@ aws ecr get-login-password --region eu-west-2 | docker login --username AWS --pa
 docker build -t secondary-cron-uk-v1 .
 docker tag secondary-cron-uk-v1:latest 824775819187.dkr.ecr.eu-west-2.amazonaws.com/secondary-cron-uk-v1:latest
 docker push 824775819187.dkr.ecr.eu-west-2.amazonaws.com/secondary-cron-uk-v1:latest
+docker image rm 824775819187.dkr.ecr.eu-west-2.amazonaws.com/secondary-cron-uk-v1:latest
