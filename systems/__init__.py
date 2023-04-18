a6fb559f5678cc27dcee98efb01a4ae7702cf2f5
Chandrakumar Murugesan <cmurugesan@chameleoncodewing.co.uk>
2023-04-18 17:06:12 +0100

    1. Removing CheckForapi process 2. All important information is removed and the process is done thru right way.

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,15 +1,49 @@
 import os
 import sys
 import logging
+import boto3
 
+from botocore.exceptions import ClientError
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from .odoo import Odoo
 
-url = os.environ['CAM_URL']
-db = os.environ['CAM_DB']
-username = os.environ['CAM_USERNAME']
-password = os.environ['CAM_PASSWORD']
+def get_secret():
+
+    secret_name = "prod/erp-uk/"
+    region_name = "eu-west-2"
+    
+    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+
+    try:
+    
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name)
+    
+        # Decrypts secret using the associated KMS key.
+        secret = get_secret_value_response['SecretString']
+        return secret
+    
+    except ClientError as e:
+        return {}
+
+
+erp_credential = get_secret()
+
+url = erp_credential.get('api_url')
+db = erp_credential.get('db')
+username = erp_credential.get('username')
+password = erp_credential.get('password')
 wait_sec = os.environ.get('AMAZON_UPDATE_WAIT_SEC', '60')
 wait_sec = int(wait_sec)
         
